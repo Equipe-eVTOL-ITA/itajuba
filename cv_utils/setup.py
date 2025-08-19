eb59ca8cb102f2f6452878931a9cbfe642a0e7fc
marconipavan <angelo.marconi.pavan@gmail.com>
2025-08-19 14:20:22 -0300

    removidos os warnigns de compilacao

--- cv_utils/setup.py
+++ cv_utils/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import os
+from glob import glob
 # setuptools é uma biblioteca python que permite a criação de pacotes
 
 package_name = 'itajuba_cv_utils' # este nome deve corresponder ao nome no package.xml
@@ -7,6 +9,17 @@ setup(
     name=package_name,
     version='0.1.0',
     packages=find_packages(),
+    data_files=[
+        # Install marker file in the package index
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        # Install package.xml
+        ('share/' + package_name, ['package.xml']),
+        # Install launch files (if they exist)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Install config files (if they exist)
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
     install_requires=['setuptools'], #, 'ultralytics'],
     zip_safe=True,
     maintainer='Angelo Marconi Pavan',
